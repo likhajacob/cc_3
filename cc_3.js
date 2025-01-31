c52b3fc70a30b47c0bf30735d7b511b56b174623
Likha Jacob <likha.jacob03@gmail.com>
2025-01-31 00:21:07 -0500

    Task 2 - modifying customer orders

--- cc_3.js
+++ cc_3.js
@@ -5,4 +5,14 @@ prices.push(56.99);
 // remove the first price from the array
 prices.shift();
 // log the updated price list to the console
-console.log(prices);ss
+console.log(prices);
+
+// Task 2 - Declare an array orders with at least 5 numerical values represnting quantities
+let orders = [5,10,15,25];
+//Increase the third order quantity by 5
+orders[2] += 5;
+//calculate the total number of all orders
+let totalorders = orders.reduce((sum,order) => sum + order, 0);
+//log the updated array and total order count to the console
+console.log(orders);
+console.log("Total order count:", totalorders);
