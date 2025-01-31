9c61b08f1e8d0022b11da56487a9f1cf3aa75c46
Likha Jacob <likha.jacob03@gmail.com>
2025-01-31 00:09:07 -0500

    Task 1 - Product price management

---
+++ cc_3.js
@@ -0,0 +1,8 @@
+// Task 1 - Declare an array with at least 5 numerical values
+let prices = [18.99, 34.99, 52.99, 19.98, 20.99];
+// Add a new price to the array
+prices.push(56.99);
+// remove the first price from the array
+prices.shift();
+// log the updated price list to the console
+console.log(prices);ss
